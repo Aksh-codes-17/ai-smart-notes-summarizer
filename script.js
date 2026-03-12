23dd78cc1130920f2251ed15a6bc780b6a1a20c2
Akshaya <s.akshaya1752007@gmail.com>
2026-03-12 20:25:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ function summarizeText(){
 
 let text = document.getElementById("notes").value;
 
+if(text.trim() === ""){
+document.getElementById("result").innerText="Please enter some notes.";
+return;
+}
+
 let sentences = text.split(".");
 
 let summary = sentences.slice(0,2).join(".");
